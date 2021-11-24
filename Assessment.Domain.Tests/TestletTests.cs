da707dd1b35ec8172b9685486d27a51508bd5dd9
Sasha Golubev <alexander.golubev@arcadia.spb.ru>
2021-11-24 09:57:38 +0300

    added not impleneted tesrt methods

---
+++ Assessment.Domain.Tests/TestletTests.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+
+namespace Assessment.Domain.Tests
+{
+    public class RandomizedTestletTests
+    {
+        [Fact]
+        public void Items_are_unique()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_first_item_is_pretest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_second_item_is_pretest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_rest_of_items_include_two_pretest()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void The_rest_of_items_include_six_operational()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
