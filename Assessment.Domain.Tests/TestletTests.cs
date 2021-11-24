2cc35782cb0a3622c26111377376c770c82cdd94
Sasha Golubev <alexander.golubev@arcadia.spb.ru>
2021-11-24 11:12:20 +0300

    implemented tests not implemented sut

--- Assessment.Domain.Tests/TestletTests.cs
+++ Assessment.Domain.Tests/TestletTests.cs
@@ -8,31 +8,67 @@ namespace Assessment.Domain.Tests
         [Fact]
         public void Items_are_unique()
         {
-            throw new NotImplementedException();
+            var testletId = Guid.NewGuid().ToString();
+            var items = new List<Item>();
+            var sut = new Testlet(testletId, items);
+
+            var randimizedItems = sut.Randomize();
+            var uniqueItemsCount = randimizedItems.Distinct().Count();
+            Assert.Equal(items.Count(), uniqueItemsCount);
         }
 
         [Fact]
         public void The_first_item_is_pretest()
         {
-            throw new NotImplementedException();
+            var testletId = Guid.NewGuid().ToString();
+            var items = new List<Item>();
+            var sut = new Testlet(testletId, items);
+
+            var randimizedItems = sut.Randomize();
+            var firstRandimizedItem = randimizedItems[0];
+
+            Assert.Equal(ItemTypeEnum.Pretest, firstRandimizedItem.ItemType);
         }
 
         [Fact]
         public void The_second_item_is_pretest()
         {
-            throw new NotImplementedException();
+            var testletId = Guid.NewGuid().ToString();
+            var items = new List<Item>();
+            var sut = new Testlet(testletId, items);
+
+            var randimizedItems = sut.Randomize();
+            var firstRandimizedItem = randimizedItems[0];
+
+            Assert.Equal(ItemTypeEnum.Pretest, firstRandimizedItem.ItemType);
         }
 
         [Fact]
         public void The_rest_of_items_include_two_pretest()
         {
-            throw new NotImplementedException();
+            var expectedRemaindingRandimizedPretestItemsCount = 2;
+            var testletId = Guid.NewGuid().ToString();
+            var items = new List<Item>();
+            var sut = new Testlet(testletId, items);
+
+            var randimizedItems = sut.Randomize();
+            var remaindingRandimizedItems = randimizedItems.Skip(2).ToList();
+            var remaindingRandimizedPretestItems = remaindingRandimizedItems.Where(item => item.ItemType == ItemTypeEnum.Pretest).ToList();
+            Assert.Equal(expectedRemaindingRandimizedPretestItemsCount, remaindingRandimizedPretestItems);
         }
 
         [Fact]
         public void The_rest_of_items_include_six_operational()
         {
-            throw new NotImplementedException();
+            var expectedRemaindingRandimizedOperationalItemsCount = 6;
+            var testletId = Guid.NewGuid().ToString();
+            var items = new List<Item>();
+            var sut = new Testlet(testletId, items);
+
+            var randimizedItems = sut.Randomize();
+            var remaindingRandimizedItems = randimizedItems.Skip(2).ToList();
+            var remaindingRandimizedOperationalItems = remaindingRandimizedItems.Where(item => item.ItemType == ItemTypeEnum.Operational).ToList();
+            Assert.Equal(expectedRemaindingRandimizedOperationalItemsCount, remaindingRandimizedOperationalItems);
         }
 
     }
